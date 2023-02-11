6030e5ee6589febc2ddcf44a7654fda0900ea742
labeneko <labecat@gmail.com>
2023-02-11 11:34:59 +0900

    パス修正

--- fetch.sh
+++ fetch.sh
@@ -16,15 +16,15 @@ currentmd5=`md5sum $savepath | awk '{ print $1 }'`
 previousmd5=`md5sum latest.jpg | awk '{ print $1 }'`
 echo $currentmd5
 echo $previousmd5
-if [[ $currentmd5 != $previousmd5 ]]; then tail -n 143 images/24h.txt >> images/24hp.txt; fi
-if [[ $currentmd5 != $previousmd5 ]]; then cp images/24hp.txt images/24h.txt; fi
-if [[ $currentmd5 != $previousmd5 ]]; then cp $savepath latest.jpg; fi
-if [[ $currentmd5 != $previousmd5 ]]; then rm images/24hp.txt; fi
+if [[ $currentmd5 != $previousmd5 ]]; then tail -n 143 "$reponame/images/24h.txt" >> "$reponame/images/24hp.txt"; fi
+if [[ $currentmd5 != $previousmd5 ]]; then cp "$reponame/images/24hp.txt" "$reponame/images/24h.txt"; fi
+if [[ $currentmd5 != $previousmd5 ]]; then cp $savepath "$reponame/latest.jpg"; fi
+if [[ $currentmd5 != $previousmd5 ]]; then rm "$reponame/images/24hp.txt"; fi
 if [[ $currentmd5 != $previousmd5 ]]; then echo "$savepath" >> "$reponame/images/24h.txt"; fi
 if [[ $currentmd5 != $previousmd5 ]]; then echo "$savepath" >> "$reponame/images/${year}/${month}/${date}/images.txt"; fi
 if [[ $currentmd5 == $previousmd5 ]]; then rm $savepath ; fi
 aws s3 cp --region ap-northeast-1 $savepath "s3://liver-camera/${savepath}"
-aws s3 cp --region ap-northeast-1 latest.jpg "s3://liver-camera/${reponame}/latest.jpg"
-aws s3 cp --region ap-northeast-1 images/24h.txt "s3://liver-camera/${reponame}/images/24h.txt"
-aws s3 cp --region ap-northeast-1 images/${year}/${month}/${date}/images.txt "s3://liver-camera/${reponame}/images/${year}/${month}/${date}/images.txt"
+aws s3 cp --region ap-northeast-1 "$reponame/latest.jpg" "s3://liver-camera/${reponame}/latest.jpg"
+aws s3 cp --region ap-northeast-1 "$reponame/images/24h.txt" "s3://liver-camera/${reponame}/images/24h.txt"
+aws s3 cp --region ap-northeast-1 "$reponame/images/${year}/${month}/${date}/images.txt" "s3://liver-camera/${reponame}/images/${year}/${month}/${date}/images.txt"
 find ./ | grep -E "[0-9]+.jpg" | xargs rm -f
