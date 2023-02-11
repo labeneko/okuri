82e3ded46d13c078867d61607db238d8956c61a4
labeneko <labecat@gmail.com>
2023-02-11 12:31:35 +0900

    10河川

--- fetch.sh
+++ fetch.sh
@@ -4,9 +4,27 @@ month=`TZ=Asia/Tokyo date '+%m'`
 date=`TZ=Asia/Tokyo date '+%d'`
 
 reponames[0]="okuri"
-urls[0]="https://www.ktr.mlit.go.jp/keihin/webcam/cam_houon-bridge_OBS.jpg?${now}"
+urls[0]="https://www.ktr.mlit.go.jp/keihin/webcam/cam_houon-bridge_OBS.jpg"
 reponames[1]="daimaru"
-urls[1]="https://www.ktr.mlit.go.jp/keihin/webcam/cam_daimaru_dam_lower.jpg?${now}"
+urls[1]="https://www.ktr.mlit.go.jp/keihin/webcam/cam_daimaru_dam_lower.jpg"
+reponames[2]="keihin00151"
+urls[2]="https://www.ktr.mlit.go.jp/keihin/webcam/cam_tama-river-Estuary_OBS.jpg"
+reponames[3]="keihin00160"
+urls[3]="https://www.ktr.mlit.go.jp/keihin/webcam/cam_dencho_branch-office_2.jpg"
+reponames[4]="keihin00161"
+urls[4]="https://www.ktr.mlit.go.jp/keihin/webcam/cam_isihara_OBS_1.jpg"
+reponames[5]="keihin00169"
+urls[5]="https://www.ktr.mlit.go.jp/keihin/webcam/cam_hino-bridge_OBS.jpg"
+reponames[6]="keihin00171"
+urls[6]="https://www.ktr.mlit.go.jp/keihin/webcam/cam_chofu-bridge_OBS.jpg"
+reponames[7]="keihin00174"
+urls[7]="https://www.ktr.mlit.go.jp/keihin/webcam/cam_asakawa-bridge_OBS.jpg"
+reponames[8]="keihin00606"
+urls[8]="https://www.ktr.mlit.go.jp/keihin/webcam/cam_nikotama_rise.jpg"
+reponames[9]="keihin00654"
+urls[9]="https://www.ktr.mlit.go.jp/keihin/webcam/cam_odakyu-bridge_upper.jpg"
+
+
 
 for i in ${!reponames[@]}
 do
@@ -18,7 +36,7 @@ do
   mkdir -p "$reponame/images/${year}"
   mkdir -p "$reponame/images/${year}/${month}"
   mkdir -p "$reponame/images/${year}/${month}/${date}"
-  curl -o $savepath $url
+  curl -o $savepath "$url?${now}"
   jpegoptim --max=70 $savepath
   currentmd5=`md5sum $savepath | awk '{ print $1 }'`
   previousmd5=`md5sum "$reponame/latest.jpg" | awk '{ print $1 }'`
