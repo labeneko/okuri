e4b3851b643c714d70614e0e3c8e093a450dc172
labeneko <labecat@gmail.com>
2023-02-15 19:07:22 +0900

    効率化

--- fetch.sh
+++ fetch.sh
@@ -82,13 +82,16 @@ do
   previousmd5=`md5sum "$reponame/latest.jpg" | awk '{ print $1 }'`
   echo $currentmd5
   echo $previousmd5
-  if [[ $currentmd5 != $previousmd5 ]]; then tail -n 143 "$reponame/images/24h.txt" >> "$reponame/images/24hp.txt"; fi
-  if [[ $currentmd5 != $previousmd5 ]]; then cp "$reponame/images/24hp.txt" "$reponame/images/24h.txt"; fi
-  if [[ $currentmd5 != $previousmd5 ]]; then cp $savepath "$reponame/latest.jpg"; fi
-  if [[ $currentmd5 != $previousmd5 ]]; then rm "$reponame/images/24hp.txt"; fi
-  if [[ $currentmd5 != $previousmd5 ]]; then echo "$savepath" >> "$reponame/images/24h.txt"; fi
-  if [[ $currentmd5 != $previousmd5 ]]; then echo "$savepath" >> "$reponame/images/${year}/${month}/${date}/images.txt"; fi
+
   if [[ $currentmd5 == $previousmd5 ]]; then rm $savepath ; fi
+  if [[ $currentmd5 == $previousmd5 ]]; then continue ; fi
+
+  tail -n 143 "$reponame/images/24h.txt" >> "$reponame/images/24hp.txt"
+  cp "$reponame/images/24hp.txt" "$reponame/images/24h.txt"
+  cp $savepath "$reponame/latest.jpg"
+  rm "$reponame/images/24hp.txt"
+  echo "$savepath" >> "$reponame/images/24h.txt"; fi
+  echo "$savepath" >> "$reponame/images/${year}/${month}/${date}/images.txt"
   aws s3 cp --region ap-northeast-1 $savepath "s3://liver-camera/${savepath}"
   aws s3 cp --region ap-northeast-1 "$reponame/latest.jpg" "s3://liver-camera/${reponame}/latest.jpg"
   aws s3 cp --region ap-northeast-1 "$reponame/images/24h.txt" "s3://liver-camera/${reponame}/images/24h.txt"
